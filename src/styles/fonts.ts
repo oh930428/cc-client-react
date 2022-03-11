ff24940b163f001fb67b6aeb6131da7ba1702edf
oh min seok <oh930428@gmail.com>
2022-03-11 19:53:01 +0900

    [style] index.ts 추가
    1. index.ts에 src/styles에 모든 파일을 import를 할때 한 문장으로 불러올 수 있게 정리
    2. colors.ts에 gray5 추가
    3. HCT, Body,Caption(Bold/Medium/Regular)로 세부적으로 나눔
    4. ICamp status 속성 추가 IComment 인터페이스 추가

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -2,13 +2,19 @@
 import { css } from "styled-components";
 
 export default {
+  //Header-Content-text
+  HCT: css`
+    font-weight: bold;
+    font-size: 48px;
+    line-height: 56px;
+    letter-spacing: -0.01em;
+  `,
   H1: css`
     font-weight: bold;
     font-size: 28px;
     line-height: 38px;
     letter-spacing: -0.01em;
   `,
-
   H2: css`
     font-weight: bold;
     font-size: 24px;
@@ -27,18 +33,50 @@ export default {
     line-height: 24px;
     letter-spacing: -0.01em;
   `,
-  Body1: css`
+  Body1_Bold: css`
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 18px;
+    letter-spacing: -0.01em;
+  `,
+  Body1_Medium: css`
+    font-weight: 500;
     font-size: 16px;
     line-height: 20px;
     letter-spacing: -0.01em;
   `,
-  Body2: css`
+  Body1_Regular: css`
+    font-weight: 300;
+    font-size: 16px;
+    line-height: 20px;
+    letter-spacing: -0.01em;
+  `,
+  Body2_Bold: css`
+    font-weight: 700;
     font-size: 14px;
     line-height: 18px;
     letter-spacing: -0.01em;
   `,
-  Caption: css`
+  Body2_Regular: css`
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 18px;
+    letter-spacing: -0.01em;
+  `,
+  Caption_Bold: css`
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: -0.01em;
+  `,
+  Caption_Medium: css`
     font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: -0.01em;
+  `,
+  Caption_Regular: css`
+    font-weight: 300;
     font-size: 12px;
     line-height: 14px;
     letter-spacing: -0.01em;
