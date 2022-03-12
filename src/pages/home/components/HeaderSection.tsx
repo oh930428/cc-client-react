1e6c349db0911e1348d2aaeb7ac213a880f6426b
oh min seok <oh930428@gmail.com>
2022-03-12 20:28:18 +0900

    [feat] responsive-ui 구현
    1. max-width:767px 기준으로 Home UI 반응형 구현
    2. scroll 이벤트 발생시 Navigaion 백그라운드 이미지 하얀색 과 로고 , 아이콘 색깔 변경

--- src/pages/home/components/HeaderSection.tsx
+++ src/pages/home/components/HeaderSection.tsx
@@ -3,17 +3,40 @@ import styled from "styled-components";
 import colors from "styles/colors";
 import fonts from "styles/fonts";
 import { mixin } from "styles";
+import { useMediaQuery } from "react-responsive";
 
 const HeaderSection = () => {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 767px)",
+  });
+
   return (
-    /* to-do: 반응형 작업 해야됨(mobile일 경우) */
-    <Container bgImg={require("assets/images/home_header_bg.png")}>
-      <div className="header-contents">
-        <div className="header-contents-title">
-          {`개발은 \n카페인과 함께`}
+    <Container
+      className={`${!isMobile && "headersection-container"}`}
+      bgImg={require("assets/images/home_header_bg.png")}
+    >
+      <div
+        className={`${
+          isMobile ? "header-contents-mobile" : "header-contents-desktop"
+        }`}
+      >
+        <div
+          className={`${
+            isMobile
+              ? "header-contents-mobile-title"
+              : "header-contents-desktop-title"
+          }`}
+        >
+          {`개발은\n카페인과 함께`}
           {/* to-do: swipper 작업 해야됨 */}
         </div>
-        <div className="header-contents-img" />
+        <div
+          className={`${
+            isMobile
+              ? "header-contents-mobile-img"
+              : "header-contents-desktop-img"
+          }`}
+        />
       </div>
     </Container>
   );
@@ -23,25 +46,50 @@ export default HeaderSection;
 
 const Container = styled.header<{ bgImg: string }>`
   background-image: url(${(props) => props.bgImg});
-  padding: 104px 16px 56px;
-  box-sizing: border-box;
-  margin-bottom: 56px;
+  padding: 104px 16px 32px;
 
-  .header-contents {
+  .headersection-container {
+    padding: 104px 16px 56px;
+    box-sizing: border-box;
+    margin-bottom: 56px;
+  }
+
+  .header-contents-mobile {
+    ${mixin.maxWidth}
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .header-contents-desktop {
     ${mixin.maxWidth}
     display: flex;
     justify-content: space-between;
     align-items: flex-end;
   }
 
-  .header-contents-title {
+  .header-contents-mobile-title {
+    color: ${colors.white};
+    ${fonts.H1};
+  }
+
+  .header-contents-mobile-img {
+    background-image: url(${require("../../../assets/images/home_thumbnail.png")});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    border-radius: 10px;
+    height: 240px;
+  }
+
+  .header-contents-desktop-title {
     flex: 1;
     white-space: pre-line;
-    color: ${colors.black};
+    color: ${colors.white};
     ${fonts.HCT};
   }
 
-  .header-contents-img {
+  .header-contents-desktop-img {
     flex: 2;
     background-image: url(${require("../../../assets/images/home_thumbnail.png")});
     background-size: cover;
