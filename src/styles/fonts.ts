05c8d4d595a61d1d5220d10c7719f451d1e6c5aa
oh min seok <oh930428@gmail.com>
2022-03-25 19:32:00 +0900

    [design] fonts.ts 파일에  letter-spacing 수정

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -7,78 +7,78 @@ export default {
     font-weight: bold;
     font-size: 48px;
     line-height: 56px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   H1: css`
     font-weight: bold;
     font-size: 28px;
     line-height: 38px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   H2: css`
     font-weight: bold;
     font-size: 24px;
     line-height: 34px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   H3: css`
     font-weight: bold;
     font-size: 20px;
     line-height: 26px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   H4: css`
     font-weight: bold;
     font-size: 18px;
     line-height: 24px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   Body1_Bold: css`
     font-weight: 700;
     font-size: 16px;
     line-height: 18px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   Body1_Medium: css`
     font-weight: 500;
     font-size: 16px;
     line-height: 20px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   Body1_Regular: css`
     font-weight: 300;
     font-size: 16px;
     line-height: 20px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   Body2_Bold: css`
     font-weight: 700;
     font-size: 14px;
     line-height: 18px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   Body2_Regular: css`
     font-weight: 300;
     font-size: 14px;
     line-height: 18px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   Caption_Bold: css`
     font-weight: 700;
     font-size: 12px;
     line-height: 14px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   Caption_Medium: css`
     font-weight: 500;
     font-size: 12px;
     line-height: 14px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
   Caption_Regular: css`
     font-weight: 300;
     font-size: 12px;
     line-height: 14px;
-    letter-spacing: -0.01em;
+    letter-spacing: -1%;
   `,
 };
