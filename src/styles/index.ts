d19c930f75362f34ab9670c1e41699789d8e1d22
oh min seok <oh930428@gmail.com>
2022-03-26 17:36:09 +0900

    [refactor] storybook 피드백 사항 해결
    1. stories 폴더에는 story 관련 파일만
    2. Icons.stories.tsx,  Inputs.stories.tsx 파일  코드 리팩토링

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,3 +2,4 @@ export { default as colors } from "./colors";
 export { default as fonts } from "./fonts";
 export { default as global } from "./global";
 export { default as mixin } from "./mixin";
+export { default as sizes } from "./sizes";
